9e82ed26456c5dcda2464a9042865861c336ec85
TamlynSpringer <tamlynspringer@gmail.com>
2023-02-09 11:28:47 +0100

    Authentication back-end. Set up server with config, controllers, model and routes. Working on JWT auth

---
+++ controllers/authController.js
@@ -0,0 +1,49 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const handleLogin = async (req, res) => {
+    const { email, pwd } = req.body;
+    if (!email || !pwd) return res.status(400).json({ 'message': 'Email and password are required.' });
+
+    const foundUser = await User.findOne({ email: email }).exec();
+    if (!foundUser) return res.sendStatus(401); //Unauthorized 
+    // evaluate password 
+    const match = await bcrypt.compare(pwd, foundUser.password);
+    if (match) {
+        const roles = Object.values(foundUser.roles).filter(Boolean);
+        // create JWTs - json web tokens - access is short & refresh is longer
+        // only store in memory (current application state), so it goes away once closed, dont store in local storage or cookies
+        const accessToken = jwt.sign(
+            {
+                "UserInfo": {
+                    "email": foundUser.email,
+                    "roles": roles
+                }
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '10s' }
+        );
+        const refreshToken = jwt.sign(
+            { "email": foundUser.email },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+        // Saving refreshToken with current user
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
+        console.log(roles);
+
+        // Creates Secure Cookie with refresh token
+        res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+
+        // Send authorization roles and access token to user
+        res.json({ roles, accessToken });
+
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = { handleLogin };
